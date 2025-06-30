233d413501c9143896a81146c11e49cd0bac6ab1
Aboelkasem Elsayed Kasem <aboelkasem8088@gmail.com>
2025-06-30 23:43:42 +0300

    The first use for the API Application

--- news_app/lib/widgets/list_down_item.dart
+++ news_app/lib/widgets/list_down_item.dart
@@ -17,27 +17,7 @@ class ListDownItem extends StatelessWidget {
           "في ملاعب غزة، تختلف قواعد اللعبة، وتعاد كتابة قوانين الاشتباك، خصوصا بعد عملية خان يونس؛ إذ لم تكن المواجهة مجرد اشتباك عسكري، بل كانت \"رمية\" محسوبة بدقة، نفذها مقاوم من كتائب القسام الذراع العسكرية لحركة حماس كما لو أنه في مباراة نهائية لكرة السلة.",
       img: "assets/pl.webp",
     ),
-    DownItemModel(
-      topTitl:
-          "Chelsea manager Enzo Maresca critcizes weather delays at Club World Cup and questions US hosting the competition",
-      downTitl:
-          r"Chelsea manager Enzo Maresca hit out at the Club World Cup organizers and suggested the United States is not a suitable host for the competition after his team’s last-16 win over Benfica was disrupted by a two-hour weather delay",
-      img: "assets/che.avif",
-    ),
-    DownItemModel(
-      topTitl:
-          "Lionel Messi to face former team PSG in Club World Cup round of 16 after Inter Miami progresses from group stage",
-      downTitl:
-          r"Lionel Messi is set for a reunion with former team Paris Saint-Germain after Inter Miami drew 2-2 with Brazilian club Palmeiras to reach the Club World Cup round of 16. Goals from Tadeo Allende and Luis Suárez appeared to have earned Miami its second win of the group stage, but Paulinho and Maurício struck for Palmeiras inside the final 10 minutes to earn a vital draw that also sent the Verdão into the knockout stages.",
-      img: "assets/messi.avif",
-    ),
-    DownItemModel(
-      topTitl:
-          "How Qatar defused Iran’s attack on the largest US-run base in the region",
-      downTitl:
-          "Top Qatari officials had been meeting with the country’s prime minister on Monday afternoon to find ways of de-escalating a conflict between Iran and Israel, when defense ministry personnel called to warn of incoming Iranian missiles.",
-      img: "assets/qatar.avif",
-    ),
+    
   ];
 
   // List<DownItemModel> items =
